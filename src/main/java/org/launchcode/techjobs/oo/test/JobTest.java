873506af7fc06fb53a0dc406fafce9b876455deb
Vit-Knysh <v.knysh85@gmail.com>
2022-08-18 00:42:58 -0400

    Added 2 tests from part 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,40 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId () {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob1.getId(), testJob2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(testJob instanceof Job);
+        assertEquals(testJob.getName(), "Product tester");
+        assertEquals(testJob.getEmployer().toString(), "ACME");
+        assertEquals(testJob.getLocation().toString(), "Desert");
+        assertEquals(testJob.getPositionType().toString(), "Quality control");
+        assertEquals(testJob.getCoreCompetency().toString(), "Persistence");
+
+
+    }
 
 }
